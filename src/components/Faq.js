3fa7bf530c913b7c9896d6570b0402c1ca7ecd32
yash sharma <76558099+yash558@users.noreply.github.com>
2023-04-21 21:49:54 +0530

    second

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -2,25 +2,25 @@ import React from 'react'
 import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons'
 import { Collapse } from "react-collapse";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-const faq = ({ open, toggle, title, desc }) => {
+const Faq = ({ open, toggle, title, desc }) => {
     return (
-
-        <div className="shadow rounded border border-gray-100 border-t-0 bg-white mt-5">
-            <div className="p-4 text-xl relative font-medium">
-                <div className="w-5/6" onClick={toggle}>
-                    {title}
+        <div className='d-flex items-center'>
+            <div className="shadow rounded border border-gray-100 border-t-0 bg-white mt-5">
+                <div className="p-4 text-xl relative font-medium">
+                    <div className="w-5/6" onClick={toggle}>
+                        {title}
+                    </div>
+                    <button aria-label='question-expanded' className='text-xl absolute top-0 right-0 p-4 focus:outline-none' onClick={toggle}>
+                        {open ? <FontAwesomeIcon icon={faChevronUp} className='w-5' /> : <FontAwesomeIcon icon={faChevronDown} />}
+                    </button>
                 </div>
-                <button aria-label='question-expanded' className='text-xl absolute top-0 right-0 p-4 focus:outline-none' onClick={toggle}>
-                    {open ? <FontAwesomeIcon icon={faChevronUp} className='w-5' /> : <FontAwesomeIcon icon={faChevronDown} />}
-                </button>
+
+                <Collapse isOpened={open}>
+                    {desc}
+                </Collapse>
             </div>
-
-            <Collapse isOpened={open}>
-                {desc}
-            </Collapse>
         </div>
-
     )
 }
 
-export default faq
+export default Faq
